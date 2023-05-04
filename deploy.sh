360f1848ffa5fc4177beb99c1e44fc5139323f35
Ben Chan <benchan79@gmail.com>
2023-05-04 20:57:06 +0800

    Edit docker tags

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@
 
 docker stop mod4-app || true
 docker rm mod4-app || true
-docker pull afawafa/mod4-project-docker:latest
-docker run -d --name mod4-app -p 8080:8080 afawafa/mod4-project-docker:latest
+docker pull benchan79/mod4-project-docker:latest
+docker run -d --name mod4-app -p 8080:8080 benchan79/mod4-project-docker:latest
